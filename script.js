f06a823d8fa4c4a8ccc33a44a98667ad90024171
Aditya Kadam <adityask2194@gmail.com>
2025-01-16 20:12:40 +0000

    JS practical session

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     handleFormSubmit()
 })
 
-function handleFormSubmit()
-{
+function handleFormSubmit() {
     // get the form element using the id from the document.
     let form = document.getElementById("username-form")
-    
-    form.addEventListener("submit", function(event){
-    //    prevent default behaviour
+
+    form.addEventListener("submit", function (event) {
+        //    prevent default behaviour
         event.preventDefault()
 
         // create a form data object providing the form element
@@ -23,23 +22,45 @@ function handleFormSubmit()
 
 
 
-function getUserDetailsFromGithub(userInput)
-{
- fetch(`https://api.github.com/users/${userInput.username}`)
- .then((response) => response.json())
- .then((data) => {
-    let userimage = document.querySelector("#userimage")
-    let public_repos = document.querySelector("#public_repos")
-    let followers = document.querySelector("#followers")
-    let usernameTag = document.querySelector("#username")
+function getUserDetailsFromGithub(userInput) {
+    // initially we add the class of hidden to the div with the id of user-info if it is not hidden
+    document.querySelector("#user-info").classList.add("hidden")
 
-    usernameTag.innerHTML = data.login
-    followers.innerHTML = data.followers
-    public_repos.innerHTML = data.public_repos
-    userimage.setAttribute("src", data.avatar_url) 
+    // initially we add the class of hidden to the div with the id of error if it is not hidden
+    document.querySelector("#error").classList.add("hidden")
 
-    console.log('data', data)
- })
+
+    fetch(`https://api.github.com/users/${userInput.username}`)
+        .then((response) => response.json())
+        .then((data) => {
+
+
+
+            // handle error messages and UI
+            if (data.message) {
+                let errorContainer = document.querySelector("#error")
+                errorContainer.classList.remove("hidden")
+                errorContainer.innerHTML = data.message
+                return
+            }
+
+
+            // remove the class of hidden from the div with the id of user-info
+            document.querySelector("#user-info").classList.remove("hidden")
+            let userimage = document.querySelector("#userimage")
+            let public_repos = document.querySelector("#public_repos")
+            let followers = document.querySelector("#followers")
+            let usernameTag = document.querySelector("#username")
+
+
+
+            usernameTag.innerHTML = data.login
+            followers.innerHTML = data.followers
+            public_repos.innerHTML = data.public_repos
+            userimage.setAttribute("src", data.avatar_url)
+
+
+        })
 }
 
 
